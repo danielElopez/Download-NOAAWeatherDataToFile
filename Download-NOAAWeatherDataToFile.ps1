e7c318a5e808cccf58540813bad53ce9614656f4
danielElopez <dlopez@osisoft.com>
2020-05-15 15:08:37 -0500

    First upload

---
+++ Download-NOAAWeatherDataToFile.ps1
@@ -0,0 +1,5 @@
+﻿Start-Transcript ".\outputlog.txt"
+$webRequestResponse = Invoke-WebRequest -Uri "https://w1.weather.gov/xml/current_obs/KMSP.xml" -ErrorAction Continue -Verbose
+$weatherText = ((Out-String -InputObject ([XML]$webRequestResponse.Content).current_observation) -replace " ", "") -replace "observation_time_rfc822:", "timestamp:"
+Out-File ".\weather_data.txt" -Encoding ascii -Force -InputObject $weatherText -ErrorAction Continue -Verbose
+Stop-Transcript
